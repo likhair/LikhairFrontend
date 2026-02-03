d3d98b31da7798a0a3de7b2f72058b244749c004
Likhair <venglikhair086@gmail.com>
2026-02-03 15:59:11 +0700

    fix base

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-   base: '/LikhairNoteProject/',
+    base: '/LikhairFrontend/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
